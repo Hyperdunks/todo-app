4d6b0b5023e1ace9189265d0fec5f6a16183f9a7
Harsh Chauhan <harshchauhan642006@gmail.com>
2025-08-19 15:53:29 +0530

    New title + TodoSchema update

--- server/db.js
+++ server/db.js
@@ -3,22 +3,48 @@ const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
 
 const userSchema = new Schema({
-	email: { type: String, unique: true },
-	username: String,
-	password: String,
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
 });
 
-const todoSchema = new Schema({
-	username: {
-		type: ObjectId,
-		ref: "user",
-	}, // a foreign key connecting both tables together
-	title: String,
-	description: String,
-	date: { type: Date, default: Date.now() },
-	status: Boolean,
-	til: Date,
-});
+const todoSchema = new Schema(
+	{
+		username: {
+			type: ObjectId,
+			ref: "user",
+			required: true,
+		}, // a foreign key connecting both tables together
+		title: {
+			type: String,
+			required: true,
+		},
+		description: String,
+		date: { type: Date, default: Date.now },
+		status: {
+			type: String,
+			enum: ["completed", "pending", "in-progress"],
+			default: "pending",
+		},
+		dueDate: Date,
+		priority: {
+			type: String,
+			enum: ["low", "medium", "high"],
+			default: "medium",
+		},
+	},
+	{ timestamps: true },
+);
 
 const userModel = mongoose.model("user", userSchema);
 const todoModel = mongoose.model("todo", todoSchema);
